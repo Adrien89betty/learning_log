bc6887cd8b268a965cd6d7a09484631b5b0f6574
adrien leveille <adrienleveille.contact@gmail.com>
2024-05-17 19:24:12 +0200

    Modifiying print_all method in seeds

--- learning_logs/seeds.py
+++ learning_logs/seeds.py
@@ -6,7 +6,7 @@ def run():
     Topic.objects.create(text='Python Basics')
     Topic.objects.create(text='Machine Learning')
 
-# def print_all():
-#     topics = Topic.objects.all()
-#     for topic in topics:
-#         print(topic.text)
+def print_all():
+    topics = Topic.objects.all()
+    for topic in topics:
+        print(topic.id, topic.text)
