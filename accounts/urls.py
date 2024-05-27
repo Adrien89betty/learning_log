276950e0340eabc90bc7b9dba311f3f4f55abde7
adrien leveille <adrienleveille.contact@gmail.com>
2024-05-27 12:18:40 +0200

    Added register function to views in accounts.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 """Defines URL patternes for accounts."""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     # Includes default auth urls.
     path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
