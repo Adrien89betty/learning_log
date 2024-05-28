4ea54eddf042259b453eb167c23cf04c8ce97c3b
adrien leveille <adrienleveille.contact@gmail.com>
2024-05-28 15:17:51 +0200

    Restricting acces to the topics page.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,3 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # My settiings.
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
